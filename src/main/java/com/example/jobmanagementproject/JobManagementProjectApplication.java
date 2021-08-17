7f28e0b6fc621aaaf0aaa9f7a352c2fc3f806a2f
Ben-Malik <benmaliktchamalam@gmail.com>
2021-08-18 02:23:27 +0300

    Job Management Project INitialized

---
+++ src/main/java/com/example/jobmanagementproject/JobManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobmanagementproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobManagementProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobManagementProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jobmanagementproject/JobManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobmanagementproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobManagementProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
